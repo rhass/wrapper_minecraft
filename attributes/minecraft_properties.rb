09aa273ee69717ba671e7e1b6d7f40cc5e7039ca
Ryan Hass <ryan@invalidchecksum.net>
2015-05-31 01:44:47 -0700

    Allow flight.

--- attributes/minecraft_properties.rb
+++ attributes/minecraft_properties.rb
@@ -1,2 +1,2 @@
-default['minecraft']['properties']['motd'] = 'Welcome to the Family Minecraft Server!'
-
+force_default['minecraft']['properties']['motd'] = 'Welcome to the Family Minecraft Server!'
+force_default['minecraft']['properties']['allow-flight'] = true
