2d1de9b0c4e8c01aa1438ef83728cd9e7d96546a
Ryan Hass <ryan@invalidchecksum.net>
2015-05-31 02:08:44 -0700

    Configure spigot.
    This is supposed to configure Spigot... other than config options I do
    not see any evidence of it actually building, downloading, installing,
    or running Spigot.

--- attributes/minecraft.rb
+++ attributes/minecraft.rb
@@ -1,3 +1,4 @@
+force_default['minecraft']['install_type']        = 'spigot'
 force_default['minecraft']['url']                 = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.8.6/minecraft_server.1.8.6.jar'
 force_default['minecraft']['checksum']            = '7fc66b2b54f0f4d65fdd6d6484a50f432c144ef02072d3435d5660f120f58e0d'
 force_default['minecraft']['accept_eula']         = 'true'
