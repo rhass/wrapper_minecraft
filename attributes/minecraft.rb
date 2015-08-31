d08f1eb620aca5bfeb31ae705aab389f6ab5217f
Ryan Hass <ryan@invalidchecksum.net>
2015-08-30 23:05:56 -0700

    Use spigot server

--- attributes/minecraft.rb
+++ attributes/minecraft.rb
@@ -3,3 +3,5 @@ force_default['minecraft']['url']                 = 'https://s3.amazonaws.com/Mi
 force_default['minecraft']['checksum']            = '7fc66b2b54f0f4d65fdd6d6484a50f432c144ef02072d3435d5660f120f58e0d'
 force_default['minecraft']['accept_eula']         = 'true'
 force_default['minecraft']['autorestart']         = false
+force_default['minecraft']['url']                 = 'http://getspigot.org/spigot18/spigot_server.jar'
+force_default['minecraft']['checksum']            = '33ff365dae28e98c2b2c12305bb1ed124f498ced59e5d107dc89fd135fb00075'
