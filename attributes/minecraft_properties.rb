add2296a9bb03fda2daee8b0382f856b44f41f37
Ryan Hass <ryan@invalidchecksum.net>
2015-08-31 00:30:22 -0700

    Set server to Adventure Mode.

--- attributes/minecraft_properties.rb
+++ attributes/minecraft_properties.rb
@@ -1,2 +1,3 @@
 force_default['minecraft']['properties']['motd'] = 'Welcome to the Family Minecraft Server!'
 force_default['minecraft']['properties']['allow-flight'] = true
+force_default['minecraft']['properties']['gamemode'] = 2
